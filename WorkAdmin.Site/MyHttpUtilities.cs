208b1b10bab9895b9868f0390868c201db184adc
Bin Zhou <zhoubinjolley@gmail.com>
2018-01-04 14:34:52 +0800

    Add files via upload

---
+++ WorkAdmin.Site/MyHttpUtilities.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WorkAdmin.Models.Entities;
+using WorkAdmin.Logic;
+
+namespace WorkAdmin.Site
+{
+    public class MyHttpUtilities
+    {
+        private User _currentUser;
+        public User CurrentUser
+        {
+            get
+            {
+                if (_currentUser == null && HttpContext.Current.User.Identity != null)
+                {
+                    string identityName = HttpContext.Current.User.Identity.Name;
+                    if (!string.IsNullOrWhiteSpace(identityName))
+                    {
+                        string loginName = identityName.Substring(identityName.IndexOf(@"\") + 1);
+                        _currentUser = UserService.GetUser(loginName);
+                    }
+                }
+                return _currentUser;
+            }
+        }
+
+        private string _loginName;
+        public string LoginName
+        {
+            get
+            {
+                if (_loginName == null && HttpContext.Current.User.Identity != null)
+                {
+                    string identityName = HttpContext.Current.User.Identity.Name;
+                    if (identityName != null)
+                        _loginName = identityName.Substring(identityName.IndexOf(@"\") + 1);
+                }
+                return _loginName;
+            }
+        }
+    }
+}
---
+++ WorkAdmin.Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WorkAdmin.Site.Startup))]
+namespace WorkAdmin.Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
